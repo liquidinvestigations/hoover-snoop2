fa73e36ec8e9a54bfcb2ab78e6d2217381d9b9a1
Alex Morega <alex@grep.ro>
2018-01-06 16:14:18 +0200

    recurse on walk

--- snoop/data/models.py
+++ snoop/data/models.py
@@ -26,7 +26,7 @@ class Collection(models.Model):
 class Directory(models.Model):
     collection = models.ForeignKey(Collection, on_delete=models.DO_NOTHING)
     name = models.CharField(max_length=255, blank=True)
-    parent_directory = models.ForeignKey('Directory', null=True, on_delete=models.DO_NOTHING)
+    parent_directory = models.ForeignKey('Directory', null=True, on_delete=models.DO_NOTHING, related_name='child_directory_set')
     container_file = models.ForeignKey('File', null=True, on_delete=models.DO_NOTHING)
 
     date_created = models.DateTimeField(auto_now_add=True)
--- snoop/data/tasks.py
+++ snoop/data/tasks.py
@@ -15,8 +15,16 @@ def walk(directory_pk):
     path = Path(directory.collection.root)
     while node.parent_directory:
         path_elements.append(node.name)
-    for node in reversed(path_elements):
-        path /= node.name
+        node = node.parent_directory
+    for name in reversed(path_elements):
+        path /= name
 
     print('path:', path)
-    #logger.info('walking %r', collection)
+    for thing in path.iterdir():
+        print(thing)
+        if thing.is_dir():
+            print('dir')
+            (child_directory, _) = directory.child_directory_set.get_or_create(collection=directory.collection, name=thing.name)
+            walk.delay(child_directory.pk)
+        else:
+            print('file')
