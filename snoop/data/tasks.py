56631487b2fb558452241958164f30337fe98942
Alex Morega <alex@grep.ro>
2018-01-06 16:18:07 +0200

    iter files

--- snoop/data/tasks.py
+++ snoop/data/tasks.py
@@ -6,19 +6,24 @@ from . import models
 logger = logging.getLogger(__name__)
 
 
-@celery.app.task
-def walk(directory_pk):
-    directory = models.Directory.objects.get(pk=directory_pk)
-
+def directory_absolute_path(directory):
     path_elements = []
     node = directory
     path = Path(directory.collection.root)
+
     while node.parent_directory:
         path_elements.append(node.name)
         node = node.parent_directory
     for name in reversed(path_elements):
         path /= name
 
+    return path
+
+
+@celery.app.task
+def walk(directory_pk):
+    directory = models.Directory.objects.get(pk=directory_pk)
+    path = directory_absolute_path(directory)
     print('path:', path)
     for thing in path.iterdir():
         print(thing)
@@ -28,3 +33,11 @@ def walk(directory_pk):
             walk.delay(child_directory.pk)
         else:
             print('file')
+            file_to_blob.delay(directory_pk, thing.name)
+
+
+@celery.app.task
+def file_to_blob(directory_pk, name):
+    directory = models.Directory.objects.get(pk=directory_pk)
+    path = directory_absolute_path(directory) / name
+    print('making blob', path)
