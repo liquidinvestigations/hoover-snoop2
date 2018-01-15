66c1fc5d0f1732a9fcb01013d4c47747f60ac892
Alex Morega <alex@grep.ro>
2018-01-15 19:53:44 +0200

    download magic file

--- snoop/data/dispatcher.py
+++ snoop/data/dispatcher.py
@@ -1,11 +1,13 @@
 import logging
 from . import models
 from .filesystem import walk
+from .magic import download_magic_definitions
 
 logger = logging.getLogger(__name__)
 
 
 def run_dispatcher():
+    download_magic_definitions()
     for collection in models.Collection.objects.all():
         [root] = collection.directory_set.filter(
             parent_directory__isnull=True,
--- snoop/data/magic.py
+++ snoop/data/magic.py
@@ -1,5 +1,11 @@
 import subprocess
 import re
+from urllib.request import urlopen
+from contextlib import closing
+from pathlib import Path
+
+MAGIC_URL = 'https://github.com/hoover/magic-definitions/raw/master/magic.mgc'
+MAGIC_FILE = Path(__file__).resolve().parent.parent.parent / 'magic.mgc'
 
 
 class Magic:
@@ -32,3 +38,14 @@ class Magic:
             self.process.stdin.write(buffer)
         except IOError:
             self.finish()
+
+
+def download_magic_definitions():
+    from .models import chunks
+    if not MAGIC_FILE.exists():
+        print("Downloading magic.mgc ...")
+        with MAGIC_FILE.open('wb') as f:
+            with closing(urlopen(MAGIC_URL)) as resp:
+                for chunk in chunks(resp):
+                    f.write(chunk)
+        print("ok")
