53632bfb3e23d9ab1522c5b55486033d28aa9e6a
Alex Morega <alex@grep.ro>
2018-01-16 17:54:27 +0200

    file admin - link to blob

--- snoop/data/admin.py
+++ snoop/data/admin.py
@@ -1,5 +1,17 @@
+from django.urls import reverse
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from . import models
+
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'blob_link']
+
+    def blob_link(self, obj):
+        url = reverse('admin:data_blob_change', args=[obj.blob.pk])
+        return mark_safe(f'<a href="{url}">{obj.blob.pk}</a>')
+
+    blob_link.short_description = 'blob'
 
 
 class BlobAdmin(admin.ModelAdmin):
@@ -10,7 +22,7 @@ class BlobAdmin(admin.ModelAdmin):
 
 admin.site.register(models.Collection)
 admin.site.register(models.Directory)
-admin.site.register(models.File)
+admin.site.register(models.File, FileAdmin)
 admin.site.register(models.Blob, BlobAdmin)
 admin.site.register(models.Task)
 admin.site.register(models.TaskDependency)
