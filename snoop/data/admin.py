5143fbb18367db3dd5e2bcc3b353e3e4baea4bc7
Alex Morega <alex@grep.ro>
2018-01-16 17:41:37 +0200

    blob admin - display, filter and search

--- snoop/data/admin.py
+++ snoop/data/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from . import models
 
+
+class BlobAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'mime_type', 'mime_encoding']
+    list_filter = ['mime_type']
+    search_fields = ['pk', 'mime_type', 'mime_encoding']
+
+
 admin.site.register(models.Collection)
 admin.site.register(models.Directory)
 admin.site.register(models.File)
-admin.site.register(models.Blob)
+admin.site.register(models.Blob, BlobAdmin)
 admin.site.register(models.Task)
 admin.site.register(models.TaskDependency)
 admin.site.register(models.Digest)
