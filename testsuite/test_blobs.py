ad2d490e1c8fad30787f02b3c283ba8dc5c07904
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2018-01-17 15:27:58 +0200

    parametrize testing for mime_type

--- testsuite/test_blobs.py
+++ testsuite/test_blobs.py
@@ -19,6 +19,7 @@ def test_make_blob_from_jpeg_file():
     assert image_blob.mime_type == 'image/jpeg'
     assert image_blob.mime_encoding == 'binary'
 
+
 def test_make_blob_from_first_eml_file():
     EML = settings.SNOOP_TESTDATA + "/data/eml-8-double-encoded/simple-encoding.eml"
     eml_blob = models.Blob.create_from_file(EML)
@@ -27,35 +28,34 @@ def test_make_blob_from_first_eml_file():
     assert eml_blob.mime_type == 'message/rfc822'
     assert eml_blob.mime_encoding == 'us-ascii'
 
-def test_all_eml_files_are_marked_as_rfc_822():
-    EML_LIST = [
-        "/data/no-extension/file_eml",
-        "/data/eml-2-attachment/message-without-subject.eml",
-        "/data/eml-2-attachment/Fwd: The American College of Thessaloniki - Greece - Tarek Kouatly <tarek@act.edu> - 2013-11-11 1622.eml",
-        "/data/eml-2-attachment/attachments-have-octet-stream-content-type.eml",
-        "/data/eml-2-attachment/FW: Invitation Fontys Open Day 2nd of February 2014 - Campus Venlo <campusvenlo@fontys.nl> - 2013-12-16 1700.eml",
-        "/data/eml-2-attachment/Urăsc canicula, e nașpa.eml",
-        "/data/eml-5-long-names/Attachments have long file names..eml",
-        "/data/eml-bom/with-bom.eml",
-        "/data/eml-1-promotional/Introducing Mapbox Android Services - Mapbox Team <newsletter@mapbox.com> - 2016-04-20 1603.eml",
-        "/data/eml-1-promotional/Machine Learning comes to CodinGame! - CodinGame Team <contact@codingame.com> - 2016-04-22 1731.eml",
-        "/data/eml-1-promotional/New on CodinGame: Check it out! - CodinGame <coders@codingame.com> - 2016-04-21 1034.eml",
-        "/data/eml-8-double-encoded/simple-encoding.eml",
-        "/data/eml-8-double-encoded/double-encoding.eml",
-        "/data/eml-3-uppercaseheaders/Fwd: The American College of Thessaloniki - Greece - Tarek Kouatly <tarek@act.edu> - 2013-11-11 1622.eml",
-        "/data/eml-9-pgp/encrypted-hushmail-knockoff.eml",
-        "/data/eml-9-pgp/encrypted-machine-learning-comes.eml",
-        "/data/eml-9-pgp/encrypted-hushmail-smashed-bytes.eml",
-    ]
-    for relative_path in EML_LIST:
-        file_path = settings.SNOOP_TESTDATA + relative_path
-        eml_blob = models.Blob.create_from_file(file_path)
-        assert eml_blob.mime_type == 'message/rfc822'
 
-def test_make_blob_from_a_partial_emlx_file():
-    EMLX = settings.SNOOP_TESTDATA + "/data/lists.mbox/F2D0D67E-7B19-4C30-B2E9-B58FE4789D51/Data/1/Messages/1498.partial.emlx"
-    emlx_blob = models.Blob.create_from_file(EMLX)
-
-    assert emlx_blob.sha256 == '1199ba0bab414a740428f604f08e4d2e1f8366a8bcc16478f82ab0e890d5de09'
-    assert emlx_blob.mime_type == 'message/x-emlx'
-    assert emlx_blob.mime_encoding == 'utf-8'
+@pytest.mark.parametrize('testdata_relative_path, expected_mime_type', [
+    # .eml: message/rfc822
+    ("/data/no-extension/file_eml", 'message/rfc822'),
+    ("/data/eml-2-attachment/message-without-subject.eml", 'message/rfc822'),
+    ("/data/eml-2-attachment/Fwd: The American College of Thessaloniki - Greece - Tarek Kouatly <tarek@act.edu> - 2013-11-11 1622.eml", 'message/rfc822'),
+    ("/data/eml-2-attachment/attachments-have-octet-stream-content-type.eml", 'message/rfc822'),
+    ("/data/eml-2-attachment/FW: Invitation Fontys Open Day 2nd of February 2014 - Campus Venlo <campusvenlo@fontys.nl> - 2013-12-16 1700.eml", 'message/rfc822'),
+    ("/data/eml-2-attachment/Urăsc canicula, e nașpa.eml", 'message/rfc822'),
+    ("/data/eml-5-long-names/Attachments have long file names..eml", 'message/rfc822'),
+    ("/data/eml-bom/with-bom.eml", 'message/rfc822'),
+    ("/data/eml-1-promotional/Introducing Mapbox Android Services - Mapbox Team <newsletter@mapbox.com> - 2016-04-20 1603.eml", 'message/rfc822'),
+    ("/data/eml-1-promotional/Machine Learning comes to CodinGame! - CodinGame Team <contact@codingame.com> - 2016-04-22 1731.eml", 'message/rfc822'),
+    ("/data/eml-1-promotional/New on CodinGame: Check it out! - CodinGame <coders@codingame.com> - 2016-04-21 1034.eml", 'message/rfc822'),
+    ("/data/eml-8-double-encoded/simple-encoding.eml", 'message/rfc822'),
+    ("/data/eml-8-double-encoded/double-encoding.eml", 'message/rfc822'),
+    ("/data/eml-3-uppercaseheaders/Fwd: The American College of Thessaloniki - Greece - Tarek Kouatly <tarek@act.edu> - 2013-11-11 1622.eml", 'message/rfc822'),
+    ("/data/eml-9-pgp/encrypted-hushmail-knockoff.eml", 'message/rfc822'),
+    ("/data/eml-9-pgp/encrypted-machine-learning-comes.eml", 'message/rfc822'),
+    ("/data/eml-9-pgp/encrypted-hushmail-smashed-bytes.eml", 'message/rfc822'),
+    # text/plain
+    ("/data/disk-files/long-filenames/Sample text file with a long filename Sample text file with a long filename Sample text file with a long filename .txt", 'text/plain'),
+    ("/data/disk-files/pdf-doc-txt/easychair.txt", 'text/plain'),
+    ("/data/words/usr-share-dict-words.txt", 'text/plain'),
+    # .emlx: message/x-emlx
+    ("/data/lists.mbox/F2D0D67E-7B19-4C30-B2E9-B58FE4789D51/Data/1/Messages/1498.partial.emlx", 'message/x-emlx'),
+])
+def test_blob_mime_types(testdata_relative_path, expected_mime_type):
+    file_path = settings.SNOOP_TESTDATA + testdata_relative_path
+    blob = models.Blob.create_from_file(file_path).mime_type
+    assert blob == expected_mime_type
