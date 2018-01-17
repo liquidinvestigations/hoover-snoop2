6292f9ee5caf08ffe7f0bf2f1d15f3de5263023b
Alex Morega <alex@grep.ro>
2018-01-17 20:19:28 +0200

    make the digest admin great again

--- snoop/data/admin.py
+++ snoop/data/admin.py
@@ -1,7 +1,12 @@
 from django.urls import reverse
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+from django.template.defaultfilters import truncatechars
 from . import models
+
+
+def short(blob_pk):
+    return f"{blob_pk[:10]}...{blob_pk[-4:]}"
 
 
 class FileAdmin(admin.ModelAdmin):
@@ -39,10 +44,28 @@ class TaskAdmin(admin.ModelAdmin):
     search_fields = ['pk', 'func', 'args']
 
 
+class DigestAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'collection', 'blob__mime_type', 'blob_link',
+                    'result_link']
+    list_filter = ['collection__name', 'blob__mime_type']
+    search_fields = ['pk', 'collection__pk', 'blob__pk', 'result__pk']
+
+    def blob__mime_type(self, obj):
+        return obj.blob.mime_type
+
+    def blob_link(self, obj):
+        url = reverse('admin:data_blob_change', args=[obj.blob.pk])
+        return mark_safe(f'<a href="{url}">{short(obj.blob.pk)}</a>')
+
+    def result_link(self, obj):
+        url = reverse('admin:data_blob_change', args=[obj.result.pk])
+        return mark_safe(f'<a href="{url}">{short(obj.result.pk)}</a>')
+
+
 admin.site.register(models.Collection)
 admin.site.register(models.Directory)
 admin.site.register(models.File, FileAdmin)
 admin.site.register(models.Blob, BlobAdmin)
 admin.site.register(models.Task, TaskAdmin)
 admin.site.register(models.TaskDependency)
-admin.site.register(models.Digest)
+admin.site.register(models.Digest, DigestAdmin)
