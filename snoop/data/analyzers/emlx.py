e502f22d9d66035a9122b9bed8c5bdda587ec420
Alex Morega <alex@grep.ro>
2018-01-17 16:23:19 +0200

    reconstruct emlx files as eml

---
+++ snoop/data/analyzers/emlx.py
@@ -0,0 +1,37 @@
+import re
+import email
+from .. import models
+
+
+def iter_parts(message, numbers=[]):
+    if message.is_multipart():
+        for n, part in enumerate(message.get_payload(), 1):
+            yield from iter_parts(part, numbers + [str(n)])
+    else:
+        yield '.'.join(numbers), message
+
+
+def reconstruct(file):
+    with file.blob.open() as f:
+        original_data = f.read()
+
+    eml_data = re.sub(rb'^\d+\s+', b'', original_data, re.MULTILINE)
+    message = email.message_from_bytes(eml_data)
+
+    for ref, part in iter_parts(message):
+        if part.get('X-Apple-Content-Length'):
+            ext = f'.{ref}.emlxpart'
+            part_name = re.sub(r'\.partial\.emlx$', ext, file.name)
+            part_file = (
+                file.parent_directory
+                .child_file_set
+                .get(name=part_name)
+            )
+            with part_file.blob.open() as f:
+                payload = f.read()
+            part.set_payload(payload)
+
+    with models.Blob.create() as output:
+        output.write(message.as_bytes())
+
+    return output.blob
