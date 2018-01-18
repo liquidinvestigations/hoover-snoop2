0ece7c1824bf404a617fa53830fd0e1178bc8219
Alex Morega <alex@grep.ro>
2018-01-18 13:12:47 +0200

    complain if `SECRET_KEY` needs to be changed

--- snoop/wsgi.py
+++ snoop/wsgi.py
@@ -4,3 +4,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snoop.settings")
 
 application = get_wsgi_application()
+
+from django.conf import settings
+from . import settings as default_settings
+if not settings.DEBUG and settings.SECRET_KEY == default_settings.SECRET_KEY:
+    raise RuntimeError("Please change the default SECRET_KEY setting")
