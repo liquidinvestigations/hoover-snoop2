1cc2237d02471ae03e00d7a718e5ab3a8d7e5a22
Alex Morega <alex@grep.ro>
2018-01-19 12:47:40 +0200

    extract `dispatch_walk_tasks` function

--- snoop/data/dispatcher.py
+++ snoop/data/dispatcher.py
@@ -6,11 +6,15 @@ from .magic import download_magic_definitions
 logger = logging.getLogger(__name__)
 
 
-def run_dispatcher():
-    download_magic_definitions()
+def dispatch_walk_tasks():
     for collection in models.Collection.objects.all():
         [root] = collection.directory_set.filter(
             parent_directory__isnull=True,
             container_file__isnull=True
         ).all()
         walk.laterz(root.pk)
+
+
+def run_dispatcher():
+    download_magic_definitions()
+    dispatch_walk_tasks()
