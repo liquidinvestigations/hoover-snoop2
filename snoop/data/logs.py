f19c5fd66594238f0bf38fcc051e3f2baaebf43b
Alex Morega <alex@grep.ro>
2018-02-14 01:45:00 +0200

    configure verbosity

--- snoop/data/logs.py
+++ snoop/data/logs.py
@@ -1,5 +1,12 @@
 import logging
 
 
-def logging_for_management_command():
-    logging.basicConfig(level=logging.INFO)
+def logging_for_management_command(verbosity=0):
+    if verbosity > 2:
+        level = logging.DEBUG
+
+    else:
+        level = logging.INFO
+
+    logging.basicConfig(level=level)
+    logging.getLogger().handlers[0].setLevel(level)
