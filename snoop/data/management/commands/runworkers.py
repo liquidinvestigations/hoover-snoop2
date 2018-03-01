8199534015258ac1ea53be0c9c3df1091cb20f4c
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2018-03-01 18:54:08 +0200

    parametrize number of workers

--- snoop/data/management/commands/runworkers.py
+++ snoop/data/management/commands/runworkers.py
@@ -4,14 +4,14 @@ from django.core.management.base import BaseCommand
 from ... import tasks
 
 
-def celery_argv():
+def celery_argv(num_workers=None):
     celery_binary = (
         subprocess.check_output(['which', 'celery'])
         .decode('latin1')
         .strip()
     )
 
-    return [
+    argv = [
         celery_binary,
         '-A', 'snoop.data',
         '--loglevel=info',
@@ -19,12 +19,21 @@ def celery_argv():
         '-Q', ','.join(tasks.shaormerie),
     ]
 
+    if num_workers:
+        argv += ['-c', num_workers]
+
+    return argv
+
 
 class Command(BaseCommand):
     help = "Run celery worker"
 
+    def add_arguments(self, parser):
+        parser.add_argument('-n', '--num-workers',
+                help="Number of workers to start")
+
     def handle(self, *args, **options):
         tasks.import_shaormas()
-        argv = celery_argv()
+        argv = celery_argv(num_workers=options.get('num_workers'))
         print('+', *argv)
         os.execv(argv[0], argv)
