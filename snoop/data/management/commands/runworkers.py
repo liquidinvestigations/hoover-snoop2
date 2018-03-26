8c4b87cee59d366be2493bfc756190664b9b2754
Alex Morega <alex@grep.ro>
2018-03-26 15:35:48 +0300

    specify which workers to run

--- snoop/data/management/commands/runworkers.py
+++ snoop/data/management/commands/runworkers.py
@@ -4,7 +4,7 @@ from django.core.management.base import BaseCommand
 from ... import tasks
 
 
-def celery_argv(num_workers=None):
+def celery_argv(num_workers, queues):
     celery_binary = (
         subprocess.check_output(['which', 'celery'])
         .decode('latin1')
@@ -16,7 +16,7 @@ def celery_argv(num_workers=None):
         '-A', 'snoop.data',
         '--loglevel=info',
         'worker',
-        '-Q', ','.join(tasks.shaormerie),
+        '-Q', ','.join(queues),
     ]
 
     if num_workers:
@@ -29,11 +29,17 @@ class Command(BaseCommand):
     help = "Run celery worker"
 
     def add_arguments(self, parser):
+        parser.add_argument('func', nargs='*',
+                help="Task types to run")
         parser.add_argument('-n', '--num-workers',
                 help="Number of workers to start")
 
     def handle(self, *args, **options):
         tasks.import_shaormas()
-        argv = celery_argv(num_workers=options.get('num_workers'))
+        queues = options.get('func') or tasks.shaormerie
+        argv = celery_argv(
+            num_workers=options.get('num_workers'),
+            queues=queues,
+        )
         print('+', *argv)
         os.execv(argv[0], argv)
