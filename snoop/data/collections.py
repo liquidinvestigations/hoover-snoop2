4900da5480212c7eb8ac39f8a6738d153ca561a3
Alex Morega <alex@grep.ro>
2018-04-11 16:41:28 +0300

    dispatch `filesystem.walk` task for the collection

--- snoop/data/collections.py
+++ snoop/data/collections.py
@@ -1,10 +1,12 @@
 from django.db import transaction
 from . import models
+from . import filesystem
 
 
 def create_collection(name, root):
     with transaction.atomic():
         collection = models.Collection.objects.create(name=name, root=root)
         root = collection.directory_set.create()
+        filesystem.walk.laterz(root.pk)
 
     return collection
