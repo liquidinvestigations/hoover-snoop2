2d9f5e5d7f28a6c77f5583f6207e9e3862f2e36e
Mugur Rus <mugur.rus@gmail.com>
2018-09-14 14:21:15 +0300

    fix workers processing tasks from other collections (#117)

--- snoop/data/management/commands/runworkers.py
+++ snoop/data/management/commands/runworkers.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from snoop.profiler import Profiler
@@ -23,8 +24,7 @@ def celery_argv(num_workers, queues):
         '-Q', ','.join(queues),
     ]
 
-    if num_workers:
-        argv += ['-c', num_workers]
+    argv += ['-c', num_workers if num_workers else str(os.cpu_count() * 2)]
 
     return argv
 
@@ -37,14 +37,21 @@ class Command(BaseCommand):
                 help="Task types to run")
         parser.add_argument('-n', '--num-workers',
                 help="Number of workers to start")
+        parser.add_argument('-p', '--prefix',
+                help="Prefix to insert to the queue name")
 
     def handle(self, *args, **options):
         with Profiler():
             tasks.import_shaormas()
+            if options.get('prefix'):
+                prefix = options['prefix']
+                settings.TASK_PREFIX = prefix
+            else:
+                prefix = settings.TASK_PREFIX
             queues = options.get('func') or tasks.shaormerie
             argv = celery_argv(
                 num_workers=options.get('num_workers'),
-                queues=queues,
+                queues=[f'{prefix}.{queue}' for queue in queues],
             )
             print('+', *argv)
             os.execv(argv[0], argv)
