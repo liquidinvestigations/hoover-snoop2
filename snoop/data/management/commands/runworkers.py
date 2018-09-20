3dab330502ce4fb5b40341d68a960397f2e4826a
Mugur Rus <mugur.rus@gmail.com>
2018-09-20 15:36:55 +0300

    limit number of worker processes

--- snoop/data/management/commands/runworkers.py
+++ snoop/data/management/commands/runworkers.py
@@ -9,7 +9,9 @@ from snoop.profiler import Profiler
 from ... import tasks
 
 
-def celery_argv(num_workers, queues):
+def celery_argv(custom_workers_no, queues):
+    max_workers_no = 90
+
     celery_binary = (
         subprocess.check_output(['which', 'celery'])
         .decode('latin1')
@@ -26,7 +28,12 @@ def celery_argv(num_workers, queues):
         '-Q', ','.join(queues),
     ]
 
-    argv += ['-c', num_workers if num_workers else str(os.cpu_count() * 4)]
+    workers_no = custom_workers_no if custom_workers_no else str(os.cpu_count() * 2)
+    if workers_no > max_workers_no:
+        print(f'Limitting the number of workers to {max_workers_no}')
+        workers_no = max_workers_no
+
+    argv += ['-c', workers_no]
 
     return argv
 
@@ -37,7 +44,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('func', nargs='*',
                 help="Task types to run")
-        parser.add_argument('-n', '--num-workers',
+        parser.add_argument('-n', '--workers-no',
                 help="Number of workers to start")
         parser.add_argument('-p', '--prefix',
                 help="Prefix to insert to the queue name")
@@ -52,7 +59,7 @@ class Command(BaseCommand):
                 prefix = settings.TASK_PREFIX
             queues = options.get('func') or tasks.shaormerie
             argv = celery_argv(
-                num_workers=options.get('num_workers'),
+                custom_workers_no=options.get('workers_no'),
                 queues=[f'{prefix}.{queue}' for queue in queues],
             )
             print('+', *argv)
