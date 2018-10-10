6f60e1ec703ecb838953d4a63e85086b9c6c1eab
Mugur Rus <mugur.rus@gmail.com>
2018-10-10 21:44:36 +0300

    check if celery idle while there are still pending tasks (#123)

--- snoop/data/management/commands/runworkers.py
+++ snoop/data/management/commands/runworkers.py
@@ -3,6 +3,7 @@ import subprocess
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
 
 from snoop.profiler import Profiler
 
@@ -42,6 +43,12 @@ def celery_argv(custom_workers_no, queues):
     return argv
 
 
+def create_procfile(celery_args):
+    with open('Procfile', 'w') as procfile:
+        out = render_to_string('snoop/Procfile', context={'workers_command': ' '.join(celery_args)})
+        procfile.write(out)
+
+
 class Command(BaseCommand):
     help = "Run celery worker"
 
@@ -64,7 +71,14 @@ class Command(BaseCommand):
             queues = options.get('func') or tasks.shaormerie
             argv = celery_argv(
                 custom_workers_no=options.get('workers_no'),
-                queues=[f'{prefix}.{queue}' for queue in queues],
+                queues=[f'{prefix}.{queue}' for queue in queues] + ['watchdog'],
             )
             print('+', *argv)
-            os.execv(argv[0], argv)
+            create_procfile(argv)
+            honcho_binary = (
+                subprocess.check_output(['which', 'honcho'])
+                .decode('latin1')
+                .strip()
+            )
+            os.execv(honcho_binary, [honcho_binary, 'start'])
+#             os.execv(argv[0], argv)
