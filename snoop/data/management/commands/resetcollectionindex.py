532f0c76c622a15e74c875e210048e04fdda3332
Mugur Rus <mugur.rus@gmail.com>
2019-01-10 16:36:46 +0200

    fix: update resetcollectionindex command

--- snoop/data/management/commands/resetcollectionindex.py
+++ snoop/data/management/commands/resetcollectionindex.py
@@ -1,17 +1,13 @@
 from django.core.management.base import BaseCommand
-from ...logs import logging_for_management_command
+
 from ... import indexing
-from ... import models
+from ...logs import logging_for_management_command
 
 
 class Command(BaseCommand):
     help = "Wipe and recreate the ElasticSearch index for a given collection"
 
-    def add_arguments(self, parser):
-        parser.add_argument('collection_name', type=str, help="Unique collection name.")
-
-    def handle(self, collection_name, *args, **options):
+    def handle(self, *args, **options):
         logging_for_management_command(options['verbosity'])
-        collection = models.Collection.objects.get(name=collection_name)
-        indexing.delete_index(collection.name)
-        indexing.create_index(collection.name)
+        indexing.delete_index()
+        indexing.create_index()
