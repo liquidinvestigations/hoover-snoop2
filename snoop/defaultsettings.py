7ac72ebd6277706fe83f8e0cf6b4e2fc61d71526
Alex Morega <alex@grep.ro>
2019-02-03 17:13:55 +0200

    shorten the hostname env var

--- snoop/defaultsettings.py
+++ snoop/defaultsettings.py
@@ -12,7 +12,7 @@ _default_secret_key = 'placeholder key for development'
 SECRET_KEY = os.environ.get('SECRET_KEY', _default_secret_key)
 
 ALLOWED_HOSTS = []
-_hostname = os.environ.get('SNOOP_HTTP_HOSTNAME')
+_hostname = os.environ.get('SNOOP_HOSTNAME')
 if _hostname:
     ALLOWED_HOSTS.append(_hostname)
 
