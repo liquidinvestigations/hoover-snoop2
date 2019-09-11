8dc2a3886d6ab2906cd96c0054b2523c20f249b0
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-09-11 23:54:45 +0300

    don't download magic, it's in the image

--- snoop/data/management/commands/initcollection.py
+++ snoop/data/management/commands/initcollection.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 
-from .magic import download_magic_definitions
 from ... import indexing, models
 from ...logs import logging_for_management_command
 
@@ -10,8 +9,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         logging_for_management_command(options['verbosity'])
-
-        download_magic_definitions()
 
         if not models.Directory.root():
             models.Directory.objects.create()
