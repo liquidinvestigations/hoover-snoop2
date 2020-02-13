b183ff483860705516d7e54bb5281a9470b2dc5b
Alex Morega <alex@grep.ro>
2020-02-13 15:23:59 +0200

    Fix flake8

--- snoop/data/models.py
+++ snoop/data/models.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import tempfile
 import hashlib
 from django.db import models
-from django.db.models.signals import post_save
 from django.conf import settings
 from django.template.defaultfilters import truncatechars
 from django.contrib.postgres.fields import JSONField
