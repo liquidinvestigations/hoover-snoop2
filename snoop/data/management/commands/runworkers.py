b41ff0b4dd5fd6546634a9fdee66f412c030e8df
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-05-05 22:42:35 +0300

    remove Procfile and honcho

--- snoop/data/management/commands/runworkers.py
+++ snoop/data/management/commands/runworkers.py
@@ -4,7 +4,6 @@ import subprocess
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.template.loader import render_to_string
 
 from snoop.profiler import Profiler
 
@@ -37,17 +36,6 @@ def celery_argv(queues):
     return argv
 
 
-def create_procfile(celery_args):
-    with open('Procfile', 'w') as procfile:
-        out = render_to_string('snoop/Procfile', context={'workers_command': ' '.join(celery_args)})
-        procfile.write(out)
-
-    with open('Procfile') as procfile:
-        for line in procfile.readlines():
-            log.info('Procfile: %s', line.strip())
-    log.info('Procfile done.')
-
-
 class Command(BaseCommand):
     help = "Run celery worker"
 
@@ -72,11 +60,5 @@ class Command(BaseCommand):
             argv = celery_argv(
                 queues=[f'{prefix}.{queue}' for queue in queues] + system_queues,
             )
-            create_procfile(argv)
-            honcho_binary = (
-                subprocess.check_output(['which', 'honcho'])
-                .decode('latin1')
-                .strip()
-            )
-            log.info('running honcho...')
-            os.execv(honcho_binary, [honcho_binary, 'start'])
+            log.debug('+' + ' '.join(argv))
+            os.execv(argv[0], argv)
