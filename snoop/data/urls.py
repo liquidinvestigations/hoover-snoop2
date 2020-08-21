dbb6afcbc49530e83f603bdaf4eac785e94be747
Marius Razvan Pricop <razvan.pricop@protonmail.com>
2020-08-21 18:52:02 +0200

    Add view and API entry for File model

--- snoop/data/urls.py
+++ snoop/data/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('<collection>/_directory_<int:pk>/json', views.directory),
+    path('<collection>/_file_<int:pk>/json', views.file_view),
     path('<collection>/<hash>/json', views.document),
     path('<collection>/<hash>/raw/<filename>', views.document_download),
     path('<collection>/<hash>/ocr/<ocrname>/', views.document_ocr),
