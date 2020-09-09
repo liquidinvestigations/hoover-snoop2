1102f60d3f104b8924e39daecac221177cb62ae2
Pricop Marius Razvan <razzvy_20@yahoo.com>
2020-09-09 12:34:20 +0200

    Adapt API to old behaviour

--- snoop/data/urls.py
+++ snoop/data/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('<collection>/_directory_<int:pk>/json', views.directory),
     path('<collection>/_file_<int:pk>/json', views.file_view),
+    path('<collection>/_file_<int:pk>/raw/<filename>', views.file_download),
+    path('<collection>/_file_<int:pk>/ocr/<ocrname>/', views.file_ocr),
+    path('<collection>/_file_<int:pk>/locations', views.file_locations),
     path('<collection>/<hash>/json', views.document),
     path('<collection>/<hash>/raw/<filename>', views.document_download),
     path('<collection>/<hash>/ocr/<ocrname>/', views.document_ocr),
