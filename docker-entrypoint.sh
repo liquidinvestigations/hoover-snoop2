316c1227a5191f2c26f20085d240ac83ef556329
Morten Stehr <morten.stehr@spiegel.de>
2020-09-22 15:31:53 +0200

    change to docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,11 @@
 echo "Running in $PWD"
 
 chown $UID:$GID -R $DATA_DIR
-chown $UID:$GID -R $DATA_DIR/blobs
+#chown $UID:$GID -R $DATA_DIR/blobs
 # mkdir $DATA_DIR/blobs/uploads
 # chown $UID:$GID -R $DATA_DIR/blobs/uploads
 # chown $UID:$GID -R $DATA_DIR/static
 
 whoami
 
-# sudo -Eu $USER_NAME "$@"
-"$@"
+sudo -Eu $USER_NAME "$@"
