6d19252736d758f0ebdee4f61f72d2b1e7e9552c
Morten Stehr <morten.stehr@spiegel.de>
2020-09-23 14:39:55 +0200

    changes for pytest

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,9 @@ echo "Running in $PWD"
 mkdir -p /opt/hoover/snoop/testsuite/volumes/snoop-pg/data
 chown $UID:$GID -R /opt/hoover/snoop/testsuite/volumes/snoop-pg/data
 
+mkdir -p /opt/hoover/snoop/static
+chown $UID:$GID -R /opt/hoover/snoop/static/
+
 chown $UID:$GID $DATA_DIR/*
 
 whoami
