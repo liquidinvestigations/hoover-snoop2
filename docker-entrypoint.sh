3a8be504f366acabe24cbc57df6bdf57b22b3f7b
Morten Stehr <morten.stehr@spiegel.de>
2020-09-29 09:41:45 +0200

    more permission changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,9 @@ echo "Running in $PWD"
 mkdir -p /opt/hoover/snoop/testsuite/volumes/snoop-pg/data
 chown $UID:$GID -R /opt/hoover/snoop/testsuite/volumes/snoop-pg/data
 
+chown -R 666:666 /opt/magic-definitions
+chown -R 666:666 /opt/libpst
+
 chown $UID:$GID $DATA_DIR/*
 
 whoami
