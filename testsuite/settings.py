4f9c60ea11f13465ea693cf541bd756af1c7fee5
Morten Stehr <morten.stehr@spiegel.de>
2020-10-07 16:58:11 +0200

    change drone.yml

--- testsuite/settings.py
+++ testsuite/settings.py
@@ -4,7 +4,7 @@ from snoop.defaultsettings import *
 
 SNOOP_TESTDATA = SNOOP_COLLECTION_ROOT + '/testdata'
 print(SNOOP_TESTDATA)
-print(os.listdir(SNOOP_TESTDATA))
+print(os.listdir('/opt/hoover/collections'))
 assert os.path.isdir(SNOOP_TESTDATA)
 
 CELERY_TASK_ALWAYS_EAGER = True
