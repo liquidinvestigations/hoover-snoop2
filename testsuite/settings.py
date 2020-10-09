89d7dbeed87eabd8828be51dafff02d0976e40ec
Morten Stehr <morten.stehr@spiegel.de>
2020-10-09 09:57:16 +0200

    changed volumes

--- testsuite/settings.py
+++ testsuite/settings.py
@@ -2,6 +2,7 @@ import os
 
 from snoop.defaultsettings import *
 
+SNOOP_COLLECTIONS_ELASTICSEARCH_URL = os.environ.get('SNOOP_ES_URL', 'http://localhost:9200')
 SNOOP_TESTDATA = SNOOP_COLLECTION_ROOT + '/testdata'
 assert os.path.isdir(SNOOP_TESTDATA)
 
