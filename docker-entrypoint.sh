7d39992ad4413461d2c4e40c7a2691d5f58172e5
Morten Stehr <morten.stehr@spiegel.de>
2020-10-15 16:25:07 +0200

    change permissions celery beat

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,8 +10,14 @@ chown -R 666:666 /opt/magic-definitions
 chown -R 666:666 /opt/libpst
 
 if [ -f ./celerybeat-schedule ]; then
-    chown 666:666 ./celerybeat-schedule
+  echo "./celerybeat-schedule found"
+  chown 666:666 ./celerybeat-schedule
 fi
+if [ -f celerybeat-schedule ]; then
+  echo "celerybeat-schedule found"
+  chown 666:666 celerybeat-schedule
+fi
+
 
 
 chown $UID:$GID $DATA_DIR/*
