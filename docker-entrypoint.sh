6a653676bd204160559ea1afca254fd9c16f60cc
Morten Stehr <morten.stehr@spiegel.de>
2020-11-04 16:43:21 +0100

    docker in docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,12 +7,8 @@ echo "Running in $PWD"
 
 chown 666:666 /opt/hoover
 chown -R 666:666 /opt/hoover/snoop
- 
+
 chown $UID:$GID $DATA_DIR/*
-
-whoami
-
-echo $SNOOP_DB
 
 exec gosu $USER_NAME "$@"
 #sudo -Eu $USER_NAME "$@"
