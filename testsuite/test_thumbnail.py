7a4a2c5e46e01b8441e3ae0b03a2c1803664974f
Kjell Knudsen <kjell.knudsen@yahoo.de>
2021-04-22 13:47:37 +0200

    added test

---
+++ testsuite/test_thumbnail.py
@@ -0,0 +1,22 @@
+import pytest
+
+from conftest import TESTDATA, CollectionApiClient
+
+pytestmark = [pytest.mark.django_db]
+
+
+def test_thumbnail_digested(fakedata, taskmanager, client):
+    root = fakedata.init()
+    test_doc = TESTDATA / './no-extension/file_doc'
+    with test_doc.open('rb') as f:
+        blob = fakedata.blob(f.read())
+    fakedata.file(root, 'file.doc', blob)
+
+    taskmanager.run()
+
+    api = CollectionApiClient(client)
+    digest = api.get_digest(blob.pk)['content']
+
+    print(digest.keys())
+
+    assert digest['has-thumbnails'] is True
