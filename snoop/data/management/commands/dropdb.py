f77edd3f94e9643e7b18df36af5849aefebcd677
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2022-05-10 18:46:25 +0300

    remove confirmation from drop_db command

--- snoop/data/management/commands/dropdb.py
+++ snoop/data/management/commands/dropdb.py
@@ -1,4 +1,4 @@
-"""Delete database for specific collection.
+"""Delete database from postgres, making sure to drop connections.
 
 This command is required because postgres 12 does not have `dropdb --force`.
 It was added in version 13, which we do not have yet.
@@ -13,29 +13,15 @@ from ... import collections
 log = logging.getLogger(__name__)
 
 
-def confirm(PROMPT):
-    return input(PROMPT).strip().lower() == 'yes'
-
-
 class Command(BaseCommand):
     "Retry running task"
 
     def add_arguments(self, parser):
-        """One flag called `--force` to avoid being asked for confirmation."""
-
         parser.add_argument('db_name', help="databasee name")
-        parser.add_argument('--force', action='store_true',
-                            help="Don't ask for confirmation")
 
-    def handle(self, db_name, force, **options):
+    def handle(self, db_name, **options):
         """Find and delete databases, indexes and blobs not bound to any collection.
         """
-
         logging_for_management_command(options['verbosity'])
 
-        PROMPT = f'POSTGRES DATABASE {db_name} WILL BE DROPPED (type "yes" to confirm):'
-        if options.get('force') or confirm(PROMPT):
-            collections.drop_db(db_name)
-        else:
-            print('Exiting without any changes.\n')
-            return
+        collections.drop_db(db_name)
