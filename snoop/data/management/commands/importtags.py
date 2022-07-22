ddce4cb273fd2432b33976e03a775dc0f2cc5b50
Kjell Knudsen <kjell.knudsen@yahoo.de>
2022-07-22 10:43:22 +0200

    add some error checking

--- snoop/data/management/commands/importtags.py
+++ snoop/data/management/commands/importtags.py
@@ -1,6 +1,8 @@
 """Command to import tags from a file."""
 import csv
+from io import StringIO
 import logging
+import traceback
 
 from django.core.management.base import BaseCommand
 from snoop.data.logs import logging_for_management_command
@@ -17,35 +19,34 @@ def clean_tag(tag):
     return cleaned_tag
 
 
-def read_csv(file_path):
-    """Read a csv containing md5 hashes and tags to import.
+def read_csv(csv_str):
+    """Read a csv from a string containing md5 hashes and tags to import.
 
     Returns: dictionary with md5 hashes as keys and a list of tags as values.
     """
+    csv_file = StringIO(csv_str)
     taglist = {}
-    with open(file_path, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file, fieldnames=['md5', 'tags'])
-        next(csv_reader, None)  # skip the headers
-        for row in csv_reader:
-            clean_tags = row['tags'].split(',')
-            clean_tags = [clean_tag(tag) for tag in clean_tags]
-            taglist[row['md5']] = clean_tags
+    csv_reader = csv.DictReader(csv_file, fieldnames=['md5', 'tags'])
+    next(csv_reader, None)  # skip the headers
+    for row in csv_reader:
+        clean_tags = row['tags'].split(',')
+        clean_tags = [clean_tag(tag) for tag in clean_tags]
+        taglist[row['md5']] = clean_tags
     return taglist
 
 
 def update_tags(md5, tags, collection, uuid, username, public=False):
     """Create document tags for all new tags from the list."""
-    collection = collections.ALL[collection]
     updated = False
     with collection.set_current():
         blob = models.Blob.objects.get(md5=md5)
         digest = models.Digest.objects.get(blob=blob.pk)
-        existing_tags = models.DocumentUserTag.objects.filter(digest=digest.pk).values_list('tag', flat=True)
+        updated = False
         for tag in tags:
-            if tag not in existing_tags:
+            _, created = models.DocumentUserTag.objects.get_or_create(digest=digest, uuid=uuid, tag=tag,
+                                                                      user=username, public=public)
+            if created:
                 updated = True
-                models.DocumentUserTag.objects.create(digest=digest, uuid=uuid, tag=tag,
-                                                      user=username, public=public)
                 log.info(f'Created new tag: "{tag}" for document: "{md5}"')
         return updated
 
@@ -54,19 +55,33 @@ class Command(BaseCommand):
     "Import Tags UUIDs for all collections. JSON content is read from stdin."
 
     def add_arguments(self, parser):
-        parser.add_argument('-c', '--col', help='collection name')
-        parser.add_argument('-t', '--tags', help='Path to csv with tags.')
-        parser.add_argument('--uuid', help='UUID of user for which the tags are imported.')
-        parser.add_argument('--user', help='Username matching the UUID.')
-        parser.add_argument('-p', action='store_true', help='Flag to set the tags as public.')
+        parser.add_argument('-c', '--collection', help='collection name', required=True)
+        parser.add_argument('-t', '--tags', help='String containing a csv with tags.', required=True)
+        parser.add_argument('--uuid', help='UUID of user for which the tags are imported.', required=True)
+        parser.add_argument('--user', help='Username matching the UUID.', required=True)
+        parser.add_argument('-p', '--public', action='store_true', help='Flag to set the tags as public.',
+                            required=True)
 
     def handle(self, **options):
         logging_for_management_command(options['verbosity'])
+        collection_name = options.get('collection')
+        try:
+            collection = collections.ALL[collection_name]
+        except KeyError:
+            log.info(f'Collection: "{collection_name}" does not exists.')
+            log.info('Exiting!')
+            return
         updated_any = False
-        for md5, tags in read_csv(options.get('tags')).items():
-            updated = update_tags(md5, tags, options.get('col'), options.get('uuid'),
-                                  options.get('user'), options.get('p'))
-            if not updated_any and updated:
-                updated_any = True
-        if not updated_any:
-            log.info('Found no new tags to update!')
+        try:
+            for md5, tags in read_csv(options.get('tags')).items():
+                updated = update_tags(md5, tags, collection, options.get('uuid'),
+                                      options.get('user'), options.get('public'))
+                if not updated_any and updated:
+                    updated_any = True
+
+            if not updated_any:
+                log.info('Found no new tags to update!')
+
+        except Exception as e:
+            log.error(f'Error updating tags: {e}')
+            log.error(traceback.format_exc())
