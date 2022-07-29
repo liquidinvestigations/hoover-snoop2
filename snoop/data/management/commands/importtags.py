ead5ed114df82f3930ac90924905580d486ed877
k-jell <42188452+k-jell@users.noreply.github.com>
2022-07-29 11:57:30 +0200

    Import tags (#471)
    * first draft import tags command and check tag name in model
    
    * get all arguments from command
    
    * add logging
    
    * params for command
    
    * add test for import command
    
    * add some error checking
    
    * add testfunction to poll for indexing status
    
    * use stdin to receive csv
    
    * use stdin to receive csv and change test
    
    * cleanup test

---
+++ snoop/data/management/commands/importtags.py
@@ -0,0 +1,86 @@
+"""Command to import tags from a file."""
+import csv
+import logging
+import traceback
+import sys
+
+from django.core.management.base import BaseCommand
+from snoop.data.logs import logging_for_management_command
+
+from ... import collections, models
+
+log = logging.getLogger(__name__)
+
+
+def clean_tag(tag):
+    """Remove whitespace from imported tag"""
+    tag_stripped = tag.strip()
+    cleaned_tag = tag_stripped.translate(str.maketrans({' ': '_', '\n': '_', '\t': '_', '\r': '_'}))
+    return cleaned_tag
+
+
+def read_csv():
+    """Read a csv from stdin containing md5 hashes and tags to import.
+
+    Returns: dictionary with md5 hashes as keys and a list of tags as values.
+    """
+    csv_file = sys.stdin
+    taglist = {}
+    csv_reader = csv.DictReader(csv_file, fieldnames=['md5', 'tags'])
+    next(csv_reader, None)  # skip the headers
+    for row in csv_reader:
+        clean_tags = row['tags'].split(',')
+        clean_tags = [clean_tag(tag) for tag in clean_tags]
+        taglist[row['md5']] = clean_tags
+    return taglist
+
+
+def update_tags(md5, tags, collection, uuid, username, public=False):
+    """Create document tags for all new tags from the list."""
+    updated = False
+    with collection.set_current():
+        blob = models.Blob.objects.get(md5=md5)
+        digest = models.Digest.objects.get(blob=blob.pk)
+        updated = False
+        for tag in tags:
+            _, created = models.DocumentUserTag.objects.get_or_create(digest=digest, uuid=uuid, tag=tag,
+                                                                      user=username, public=public)
+            if created:
+                updated = True
+                log.info(f'Created new tag: "{tag}" for document: "{md5}"')
+        return updated
+
+
+class Command(BaseCommand):
+    """Import Tags UUIDs for all collections. CSV content is read from stdin."""
+
+    def add_arguments(self, parser):
+        parser.add_argument('-c', '--collection', help='collection name', required=True)
+        parser.add_argument('--uuid', help='UUID of user for which the tags are imported.', required=True)
+        parser.add_argument('--user', help='Username matching the UUID.', required=True)
+        parser.add_argument('-p', '--public', action='store_true', help='Flag to set the tags as public.')
+
+    def handle(self, **options):
+        """Updates the tags for the user and adds new tags."""
+        logging_for_management_command(options['verbosity'])
+        collection_name = options.get('collection')
+        try:
+            collection = collections.ALL[collection_name]
+        except KeyError:
+            log.info(f'Collection: "{collection_name}" does not exists.')
+            log.info('Exiting!')
+            return
+        updated_any = False
+        try:
+            for md5, tags in read_csv().items():
+                updated = update_tags(md5, tags, collection, options.get('uuid'),
+                                      options.get('user'), options.get('public'))
+                if not updated_any and updated:
+                    updated_any = True
+
+            if not updated_any:
+                log.info('Found no new tags to update!')
+
+        except Exception as e:
+            log.error(f'Error updating tags: {e}')
+            log.error(traceback.format_exc())
---
+++ testsuite/test_tagimport.py
@@ -0,0 +1,103 @@
+import pytest
+import csv
+from snoop.data import collections, models
+from conftest import TESTDATA
+from django.conf import settings
+from django.core.management import call_command
+import requests
+import time
+import sys
+from io import StringIO
+
+ES_URL = settings.SNOOP_COLLECTIONS_ELASTICSEARCH_URL
+
+pytestmark = [pytest.mark.django_db]
+
+
+def test_tags_api(fakedata, taskmanager, client, django_user_model):
+    root = fakedata.init()
+
+    testfile1 = (TESTDATA / './no-extension/file_pdf')
+    testfile2 = (TESTDATA / './disk-files/images/bikes.jpg')
+
+    with testfile1.open('rb') as f:
+        blob1 = fakedata.blob(f.read())
+
+    with testfile2.open('rb') as f:
+        blob2 = fakedata.blob(f.read())
+
+    fakedata.file(root, 'file1.pdf', blob1)
+    fakedata.file(root, 'file2.jpg', blob2)
+
+    taskmanager.run(limit=3000)
+
+    tags_mapping = {
+        blob1.md5: ['tag1', 'tag2'],
+        blob2.md5: ['tag1', 'tag3']
+    }
+
+    col = collections.current().name
+    user = django_user_model.objects.create_user(username='test', password='pw')
+
+    create_tags_csv(tags_mapping)
+
+    call_command('importtags', collection=col, user=user.username, uuid='xyz-123', public=True)
+    digest1 = models.Digest.objects.get(blob_id=blob1.pk)
+    digest2 = models.Digest.objects.get(blob_id=blob2.pk)
+
+    assert models.DocumentUserTag.objects.filter(digest_id=digest1.pk, tag='tag1').exists()
+    assert models.DocumentUserTag.objects.filter(digest_id=digest1.pk, tag='tag2').exists()
+    assert models.DocumentUserTag.objects.filter(digest_id=digest2.pk, tag='tag1').exists()
+    assert models.DocumentUserTag.objects.filter(digest_id=digest2.pk, tag='tag3').exists()
+
+    res1 = query_es_tag('tag2').json()
+
+    # polling for tags
+    start = time.time()
+    while not res1['hits']['hits']:
+        if time.time() - start >= 300:
+            raise Exception('Indexing tags timed out!')
+        time.sleep(1)
+        res1 = query_es_tag('tag2').json()
+
+    tags1 = res1['hits']['hits'][0]['_source']['tags']
+    assert 'tag1' in tags1 and 'tag2' in tags1
+
+    res2 = query_es_tag('tag3').json()
+    tags2 = res2['hits']['hits'][0]['_source']['tags']
+    assert 'tag1' in tags2 and 'tag3' in tags2
+
+
+def create_tags_csv(tags_mapping):
+    '''Create a csv with tags to import and save it as stdin.
+
+    Takes a dictionary in the form of {blob: [tag1, tag2]}
+    as input.
+    '''
+    new_stdin = StringIO()
+    header = ['MD5 Hash', 'Tags']
+    writer = csv.writer(new_stdin)
+    writer.writerow(header)
+    for blob in tags_mapping:
+        # create a list to write to the file
+        # the format is [blob_hash, 'tag1, tag2']
+        data = [blob] + [', '.join(tags_mapping[blob])]
+        writer.writerow(data)
+
+    # management commands reads from stdin so we set it here
+    sys.stdin = StringIO(new_stdin.getvalue())
+
+
+def query_es_tag(tag):
+    """Query elasticsearch for tag and return response."""
+    es_index = collections.current().es_index
+    url = f'{ES_URL}/{es_index}/_search'
+    query = {"query": {
+        "query_string": {
+            "query": tag,
+            "default_field": "tags"
+        }
+    }}
+    res = requests.get(url=url, headers={'Content-Type': 'application/json'}, json=query)
+    assert res.status_code == 200
+    return res
