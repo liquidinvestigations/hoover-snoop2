de1095a40a8c52a134d50f21d90fa558b62512e7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-11 03:27:45 +0000

    Adding tarfile member sanitization to extractall()

--- snoop/data/indexing.py
+++ snoop/data/indexing.py
@@ -368,7 +368,29 @@ def import_index(delete=False, stream=None):
         log.info('Unpack tar archive')
 
         with tarfile.open(mode='r|*', fileobj=stream or sys.stdin.buffer) as tar:
-            tar.extractall(repo_path)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, repo_path)
 
         snapshots_resp = requests.get(f'{repo}/*')
         check_response(snapshots_resp)
