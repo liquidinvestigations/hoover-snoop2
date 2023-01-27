60edfe7cc85d524e7ed2b2b9e3d75cdec814437a
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2023-01-27 12:19:58 +0200

    instrument celery

--- snoop/data/celery.py
+++ snoop/data/celery.py
@@ -6,9 +6,17 @@ Logging and Settings for Celery are all handled here.
 import logging
 import os
 
+from opentelemetry.instrumentation.celery import CeleryInstrumentor
 from celery import Celery
+from celery.signals import worker_process_init
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "snoop.defaultsettings")
+
+
+@worker_process_init.connect(weak=False)
+def init_celery_tracing(*args, **kwargs):
+    CeleryInstrumentor().instrument()
+
 
 app = Celery('snoop.data')
 app.conf.update(
