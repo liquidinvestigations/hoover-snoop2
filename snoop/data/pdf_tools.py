9783a8094274b4b2e563c7c22855aa4b51e95831
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2023-11-15 16:34:26 +0200

    pdf tools: fix for pdf with spaces in filename

--- snoop/data/pdf_tools.py
+++ snoop/data/pdf_tools.py
@@ -22,7 +22,7 @@ def run_script(script, timeout='120s', kill='130s'):
     # qpdf is sometimes very spammy with content warnings
     with TemporaryDirectory(prefix='pdf-tools-pwd-') as pwd:
         # script = script + ' 2> >(head -c2000 >&2)'
-        script = f'cd {pwd}; ' + script
+        script = f"cd '{pwd}'; " + script
         cmd = ['/usr/bin/timeout', '-k', kill, timeout, '/bin/bash', '-exo', 'pipefail', '-c', script]
         log.warning('+ %s', script)
         return subprocess.check_output(cmd, cwd=pwd)
@@ -32,10 +32,13 @@ def get_pdf_info(path):
     """streaming wrapper to extract pdf info json (page count, chunks)"""
 #    script = "export JAVA_TOOL_OPTIONS='-Xmx3g'; pdftk - dump_data | grep NumberOfPages | head -n1"
     # script = "pdfinfo -  | grep Pages | head -n1"
-    script = f"qpdf --show-npages {path}"
+    script = f"qpdf --show-npages '{path}'"
     page_count = int(run_script(script).decode('ascii'))
     size_mb = round(os.stat(path).st_size / 2**20, 3)
-    DESIRED_CHUNK_MB = 25
+    if size_mb > 100:
+        DESIRED_CHUNK_MB = 25
+    else:
+        DESIRED_CHUNK_MB = 100
     chunk_count = max(1, int(math.ceil(size_mb / DESIRED_CHUNK_MB)))
     pages_per_chunk = int(math.ceil((page_count + 1) / chunk_count))
     pages_per_chunk = min(pages_per_chunk, MAX_PDF_PAGES_PER_CHUNK)
@@ -61,14 +64,14 @@ def split_pdf_file(path, _range, dest_path):
         " qpdf --empty --no-warn --warning-exit-0 --deterministic-id "
         " --object-streams=generate  --remove-unreferenced-resources=yes "
         " --no-original-object-ids "
-        f" --pages {path} {_range}  -- {dest_path}"
+        f" --pages '{path}' {_range}  -- '{dest_path}'"
     )
     run_script(script)
 
 
 def pdf_extract_text(infile, outfile):
     """Extract pdf text using javascript."""
-    script = f'/opt/hoover/snoop/pdf-tools/run.sh {infile} {outfile}'
+    script = f"/opt/hoover/snoop/pdf-tools/run.sh '{infile}' '{outfile}'"
     run_script(script)
 
 
